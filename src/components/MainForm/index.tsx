5e2c5920fdb8323732c8c5d08fd76c6d061ded47
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-05-07 18:44:31 -0300

    feat: implements the formatSecondsToMinutes, getNextCycles and getNextCyclesType utils and Cycles logic

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,15 +1,61 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useRef } from "react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  // const [taskName, setTaskName] = useState(''); 
+  const taskName = useRef<HTMLInputElement>(null);
+  
+  const {state, setState} = useTaskContext();
+
+  const nextCycle = getNextCycle(state.currentCycle);  
+
+  const nextCycleType = getNextCycleType(nextCycle)
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if(taskName.current === null) return;
+    
+    const taskNameValue = taskName.current.value.trim();
+    
+    if(!taskNameValue) {
+      alert('Digite o nome da tarefa!');
+      return;
+    } 
+      
+    const newTask : TaskModel = {
+      id: Date.now().toString(),
+      name: taskNameValue,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+        config: {...prevState.config}
+      };
+    })
+
   };
 
   return(
@@ -20,8 +66,9 @@ export function MainForm() {
           id="meuInput"
           type="text"
           placeholder="Digite algo"
-          value={taskName}
-          onChange={(event) => setTaskName(event.target.value)}
+          // value={taskName}
+          // onChange={(event) => setTaskName(event.target.value)}
+          ref={taskName}
         />
       </div>
 
@@ -31,9 +78,11 @@ export function MainForm() {
         </p>
       </div>    
       
-      <div className="formRow">
-        <Cycles/>
-      </div>    
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles/>
+        </div>    
+      )}
       
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon/>} />
