bacb6126ca9d6ac2315cb90e8543e68e8ad52cd4
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-05-07 16:06:02 -0300

    feat: implements the TaskContext and update  Home, Countdown and MainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,36 +1,44 @@
+import { useState } from "react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { HomeProps } from "../../pages/Home";
 
 import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 
-export function MainForm({state}: HomeProps) {
-      return(
-       <form action="" className="form">
-          <div className="formRow">
-            <DefaultInput
-              labelText="Task"
-              id="meuInput"
-              type="text"
-              placeholder="Digite algo"
-            />
-          </div>
+export function MainForm() {
+  const [taskName, setTaskName] = useState('');
 
-          <div className="formRow">
-            <p>
-              Próximo intervalo é de {state.config.workTime} minutos.
-            </p>
-          </div>    
-          
-          <div className="formRow">
-            <Cycles/>
-          </div>    
-          
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon/>} />
-            <DefaultButton icon={<StopCircleIcon/>} color='red' />
-          </div>    
-        </form>
-      );
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  };
+
+  return(
+    <form onSubmit={handleCreateNewTask} action="" className="form">
+      <div className="formRow">
+        <DefaultInput
+          labelText="Task"
+          id="meuInput"
+          type="text"
+          placeholder="Digite algo"
+          value={taskName}
+          onChange={(event) => setTaskName(event.target.value)}
+        />
+      </div>
+
+      <div className="formRow">
+        <p>
+          Próximo intervalo é de 25 minutos.
+        </p>
+      </div>    
+      
+      <div className="formRow">
+        <Cycles/>
+      </div>    
+      
+      <div className="formRow">
+        <DefaultButton icon={<PlayCircleIcon/>} />
+        <DefaultButton icon={<StopCircleIcon/>} color='red' />
+      </div>    
+    </form>
+  );
 };
