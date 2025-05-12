6b6646be447aac584fb8e13b38efad73ef87b561
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-05-12 15:24:10 -0300

    feat: includes data values on the history table from the state and includes format utils

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,8 +6,13 @@ import { DefaultButton } from "../../components/DefaultButton";
 import { TrashIcon } from "lucide-react";
 
 import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { formatDate } from "../../utils/formatDate";
+import { getTaskStatus } from "../../utils/getTaskStatus";
 
 export function History() {
+  const {state} = useTaskContext();
+
   return (
     <MainTemplate>
       <Container>
@@ -37,15 +42,23 @@ export function History() {
               </tr>
             </thead>
             <tbody>
-              {Array.from({ length: 10 }).map((_, index) => (
-                <tr key={index}>
-                  <td>Estudar React</td>
-                  <td>2h</td>
-                  <td>01/01/2023</td>
-                  <td>Concluído</td>
-                  <td>Estudo</td>
-                </tr>
-              ))}
+              {state.tasks.map((task) => {
+                const taskTypeDictionary = {
+                  workTime: "Foco",
+                  shortBreakTime: "Descanso curto",
+                  longBreakTime: "Descanso longo",
+                }
+
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
